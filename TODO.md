94466023ef1a4de736d073d6d54e5cab0f31a3e4
bladeacer <wg.nick.exe@gmail.com>
2025-05-28 22:44:56 +0800

    Fresh start

---
+++ TODO.md
@@ -0,0 +1,4 @@
+
+- [ ] Bring over `flexcyon` theme colors here
+- [ ] Modify styling to emulate themes
+- [ ] Add content to the site so it is not so stale
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,9 @@
 // @ts-check
-import starlight from '@astrojs/starlight';
 import { defineConfig } from 'astro/config';
-import starlightThemeFlexoki from 'starlight-theme-flexoki'
 
 export default defineConfig({
     outDir: 'public',
     publicDir: 'static',
     site: 'https://bladeacer.gitlab.io',
-    integrations: [
-        starlight({
-            plugins: [starlightThemeFlexoki()],
-            title: 'bladeacer\'s Portfolio',
-        }),
-    ],
+    base: '/portfolio'
 });
