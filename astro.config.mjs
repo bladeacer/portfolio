cc3adb5485630841ce2ae1edf1100bcbf3d763f1
bladeacer <wg.nick.exe@gmail.com>
2025-05-28 17:42:58 +0800

    Added flexoki theme and starlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 // @ts-check
+import starlight from '@astrojs/starlight';
 import { defineConfig } from 'astro/config';
+import starlightThemeFlexoki from 'starlight-theme-flexoki'
 
 export default defineConfig({
     outDir: 'public',
     publicDir: 'static',
+    integrations: [
+        starlight({
+            plugins: [starlightThemeFlexoki()],
+            title: 'bladeacer\'s Portfolio',
+        }),
+    ],
 });
