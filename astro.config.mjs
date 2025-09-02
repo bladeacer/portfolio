26d4fb536f03c11a182e26f449deb47bc681f1b2
bladeacer <wg.nick.exe@gmail.com>
2025-09-02 12:27:57 +0800

    Added Credits

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { tableOfContents } from 'astro-table-of-contents';
 
 export default defineConfig({
     outDir: 'public',
     publicDir: 'static',
     site: 'https://bladeacer.gitlab.io',
-    base: '/portfolio'
+    base: '/portfolio',
+    integrations: [
+        tableOfContents({
+            title: 'Table Of Contents',
+        }),
+    ],
 });
